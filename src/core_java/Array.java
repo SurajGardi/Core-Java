2058d9a7c19bd0b1838a3040e89a561a71a16d2e
SurajGardi <surajgardi0707@gmail.com>
2024-10-12 23:54:08 +0530

    array added

---
+++ src/core_java/Array.java
@@ -0,0 +1,25 @@
+package core_java;
+
+public class Array {
+
+	//Array..?
+	//==> Array is collection of similar data types..
+	
+	//	  Array are use to store multiple values in single variable, instead of 
+	//    declaring separate variables for each value
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a= {10,20,30,40};
+//		int [] a=new int[6];
+//		a[0]=10;
+//		a[1]=20;
+//		a[2]=30;
+//		a[3]=40;
+		
+		for(int i=0; i<a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
+}
