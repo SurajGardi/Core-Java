893a860d3c476944a3d4e1cf9db6c5e82497b689
SurajGardi <surajgardi0707@gmail.com>
2024-10-13 23:26:22 +0530

    star pattern-2 added

---
+++ src/core_java/forPattern1.java
@@ -0,0 +1,23 @@
+package core_java;
+
+public class forPattern1 {
+	
+//	*
+//	**
+//	***
+//	****
+//	*****
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
