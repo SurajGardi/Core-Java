c66be0cb7c177f722e8634102c1b60ccda1e2e1f
SurajGardi <surajgardi0707@gmail.com>
2024-10-14 23:04:12 +0530

    break statement added

---
+++ src/core_java/forLoopBreak.java
@@ -0,0 +1,17 @@
+package core_java;
+
+public class forLoopBreak {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0; i<=5; i++) {
+			if(i==3) {
+				break;
+			}
+			System.out.println(i);
+		}
+		System.out.println("Finish Loop");
+	}
+
+}
