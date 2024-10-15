d5780997e4e24ba26e2dfe66f5c0613ead41d536
SurajGardi <surajgardi0707@gmail.com>
2024-10-15 22:59:29 +0530

    return statement added

---
+++ src/core_java/forReturnStmt.java
@@ -0,0 +1,20 @@
+package core_java;
+
+public class forReturnStmt {
+
+	//What is Method.?
+	//==>Method is the Block of code when we required we can call..
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=add(3,5);
+		System.out.println(x);
+}
+		
+		static int add(int i,int j) {
+			int sum=i+j;//3+5=8
+		return sum;
+	}
+
+}
