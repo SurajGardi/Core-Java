0837e676314fc6fee29cd2d38e2295dd55eb6984
SurajGardi <surajgardi0707@gmail.com>
2024-10-16 22:55:49 +0530

    parameterized constructor added

---
+++ src/core_java/OOPSClassObject_ParaConstructor.java
@@ -0,0 +1,18 @@
+package core_java;
+
+//This is PARAMETERIZED Constructor
+
+public class OOPSClassObject_ParaConstructor {
+	
+	public OOPSClassObject_ParaConstructor(int a) {
+		System.out.println("Parameterized Constructor : "+a);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		OOPSClassObject_ParaConstructor t=new OOPSClassObject_ParaConstructor(10);
+	}
+
+}
