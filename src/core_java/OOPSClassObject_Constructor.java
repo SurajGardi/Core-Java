f53127b38d7a02aa833c9568e3364ab0d3308e39
SurajGardi <surajgardi0707@gmail.com>
2024-10-16 22:54:10 +0530

    defult constructor added

---
+++ src/core_java/OOPSClassObject_Constructor.java
@@ -0,0 +1,17 @@
+package core_java;
+
+public class OOPSClassObject_Constructor {
+	
+	//Here Compiler Creates The DEFAULT CONSTRUCTOR..
+	
+	int i;
+	String name;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OOPSClassObject_Constructor t=new OOPSClassObject_Constructor();
+		System.out.println(t.i+" "+t.name);
+
+	}
+
+}
