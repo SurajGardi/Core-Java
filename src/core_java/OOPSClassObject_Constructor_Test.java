13b8a733d128b971d49bd10bff56b06a5f76c530
SurajGardi <surajgardi0707@gmail.com>
2024-10-17 23:57:50 +0530

    no-argument constructor added

---
+++ src/core_java/OOPSClassObject_Constructor_Test.java
@@ -0,0 +1,18 @@
+package core_java;
+
+public class OOPSClassObject_Constructor_Test {
+	
+	//This is NO-ARGUMENT Constructor
+	
+	public OOPSClassObject_Constructor_Test() {
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OOPSClassObject_Constructor_Test t=new OOPSClassObject_Constructor_Test();
+		
+
+	}
+
+}
