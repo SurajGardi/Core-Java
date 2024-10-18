ebe14ac1bcdf318a793469175a2a40ff68570314
SurajGardi <surajgardi0707@gmail.com>
2024-10-18 23:04:36 +0530

    class object demo1 added

---
+++ src/core_java/OOPSClassObject_Bird.java
@@ -0,0 +1,15 @@
+package core_java;
+
+public class OOPSClassObject_Bird {
+
+	public void fly() {
+		System.out.println("Im Flying..");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		OOPSClassObject_Bird sparrow= new OOPSClassObject_Bird();
+		sparrow.fly();
+	}
+
+}
