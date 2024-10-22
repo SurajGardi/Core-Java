14b91aab2ac0622cbef35cb5d7f5e92bedddffaa
SurajGardi <surajgardi0707@gmail.com>
2024-10-22 22:32:59 +0530

    simple interest added

---
+++ src/core_java/SimpleInterest.java
@@ -0,0 +1,16 @@
+package core_java;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int p=20000, r=8, n=3;
+		
+		int si=p*r*n/100;
+		
+		System.out.println("Simple Interest is :"+si);
+
+	}
+
+}
