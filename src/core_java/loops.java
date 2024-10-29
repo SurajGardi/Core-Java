0c87de2bcc67b917d6657aaccc8cd775f046bcca
SurajGardi <surajgardi0707@gmail.com>
2024-10-29 22:34:22 +0530

    loop types added

---
+++ src/core_java/loops.java
@@ -0,0 +1,60 @@
+package core_java;
+
+public class loops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//**LOOPS**
+				//1.for
+				//2.while
+				//3.do while
+
+				//**for(initialization; condition; increament)
+				
+				//Using FOR Loop..
+				/*int n=5;
+				for(int i=1; i<=10;i++) {
+					System.out.println(n+ "*" + i + " = " +n*i);
+				}*/
+				
+				
+				
+				//**while->
+						//initialization
+						//while(condition)
+						//	{
+						//		code;
+						//		i++;        increment/decrement
+						//	}
+				
+				
+				//Using WHILE Loop..
+				/*int i=1;
+				while(i<=5)
+				{
+					i++;
+					System.out.println("hello");
+				}*/
+				
+				
+				
+				
+				//**do while
+						//initialization
+						//do
+						//	{
+						//		code;
+						//		increment/decrement;
+						//	}while(condition);
+				
+				//Using DO WHILE
+				int i=1;
+				do
+				{
+					System.out.println("Hello");
+					i++;
+				}while(i<=5);
+	}
+
+}
