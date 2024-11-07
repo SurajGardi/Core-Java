e0da2b78f79b310a388248a9cd5b3988cf518603
SurajGardi <surajgardi0707@gmail.com>
2024-11-07 21:54:06 +0530

    string concat() added

---
+++ src/core_java/StringConcatMethod.java
@@ -0,0 +1,16 @@
+package core_java;
+
+public class StringConcatMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//joins the two strings
+		//Returns string value
+		
+		String s1="Java";
+		String s2=" Programming";
+		System.out.println(s1.concat(s2));
+	}
+
+}
