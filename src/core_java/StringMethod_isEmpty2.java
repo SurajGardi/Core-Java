c81297e3aec88caa8204cdab45b33c7b28d57435
SurajGardi <surajgardi0707@gmail.com>
2024-11-07 21:51:46 +0530

    string isEmpty() added

---
+++ src/core_java/StringMethod_isEmpty2.java
@@ -0,0 +1,17 @@
+package core_java;
+
+public class StringMethod_isEmpty2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="Suraj";
+		if(s1.isEmpty()==true) {	
+			//if(s1.length()==0) {
+			System.out.println("String S1 is Empty..");
+		}else {
+			System.out.println("String s1 is not Empty..");
+		}
+	}
+
+}
