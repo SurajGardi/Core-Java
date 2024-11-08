73e2cdc48f0da2f0b782555e81509ff6bcb0e8c0
SurajGardi <surajgardi0707@gmail.com>
2024-11-08 23:03:44 +0530

    sring charAt() added

---
+++ src/core_java/StringMethod_charAt.java
@@ -0,0 +1,18 @@
+package core_java;
+
+public class StringMethod_charAt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//charAt() ==>  returns character at the specified index
+		//the Index value should be between Zero and length()-1.
+		
+		//Returns --> Character value
+		
+		String s1= "Suraj";
+		System.out.println(s1.length());
+		System.out.println(s1.charAt(4));
+	}
+
+}
