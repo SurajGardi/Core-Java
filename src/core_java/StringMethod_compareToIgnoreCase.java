702d74c6be69868cd0fbc2c13a590b4910225d37
SurajGardi <surajgardi0707@gmail.com>
2024-11-09 20:35:42 +0530

    string compareToIgnoreCase() added

---
+++ src/core_java/StringMethod_compareToIgnoreCase.java
@@ -0,0 +1,29 @@
+package core_java;
+
+public class StringMethod_compareToIgnoreCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//compareToIgnoreCase() ==> it ignores the UpperCase & lowerCase and returns difference between two stings
+		
+		String s1="D";//97
+		String s2="B";//65     97-65=32
+				
+		System.out.println(s1.compareToIgnoreCase(s2));
+	}
+
+}
+
+
+//Additional programs
+
+//1.WAP to compare two strings lexicographically
+//2.WAP
+//3.WAP to remove leading and trailing whiteSpaces in string  ==> trim()
+//4.WAP 
+//5.WAP
+//6.WAp to concatenate to given string to the end of another string 
+//7.WAP to compare a given strings to the specify character sequence  ==> 
+//8.WAP to check whether two string object contain the same data.
+//WAP to get the character at the given index within the string
