80d44b6d79bec5b1eb07b5cc39c3ff9a42138cf8
SurajGardi <surajgardi0707@gmail.com>
2024-11-10 22:10:06 +0530

    string equals() added

---
+++ src/core_java/StringMethod_equals.java
@@ -0,0 +1,17 @@
+package core_java;
+
+public class StringMethod_equals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//equals() ==> checks the both strings contains same elements also checks UpperCase & LowerCase of elements
+					//if elements are SAME returns boolean value --> TRUE
+					//
+		
+		String s1 = "Suraj";
+		String s2 = "suraj";
+		System.out.println(s1.equals(s2));
+	}
+
+}
