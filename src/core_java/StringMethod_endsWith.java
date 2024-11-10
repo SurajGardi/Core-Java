b498a86b5c8099c782b8ee52f80647c49a717f29
SurajGardi <surajgardi0707@gmail.com>
2024-11-10 22:07:56 +0530

    string endsWith() added

---
+++ src/core_java/StringMethod_endsWith.java
@@ -0,0 +1,13 @@
+package core_java;
+
+public class StringMethod_endsWith {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="Welcome in java";
+		boolean result=s1.endsWith("a");//checks ending character and returns boolean value
+		System.out.println(result);
+	}
+
+}
