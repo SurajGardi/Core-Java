a1c54c562a567d39bb452096146b58555bddff28
SurajGardi <surajgardi0707@gmail.com>
2024-11-12 19:49:26 +0530

    string replace() added

---
+++ src/core_java/StringMethod_replace.java
@@ -0,0 +1,13 @@
+package core_java;
+
+public class StringMethod_replace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="java Welcome in java java";
+		String result=s1.replace("java", "program");
+		System.out.println(result);
+	}
+
+}
