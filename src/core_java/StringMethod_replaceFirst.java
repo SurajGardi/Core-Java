074cfc53f0affef66c8b3a45b4d628b3bc6b9f10
SurajGardi <surajgardi0707@gmail.com>
2024-11-13 21:53:10 +0530

    string replaceFirst() added

---
+++ src/core_java/StringMethod_replaceFirst.java
@@ -0,0 +1,13 @@
+package core_java;
+
+public class StringMethod_replaceFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="java Welcome in java java";
+		String result=s1.replaceFirst("java", "program");//replaces 1st similar character/word
+		System.out.println(result);
+	}
+
+}
