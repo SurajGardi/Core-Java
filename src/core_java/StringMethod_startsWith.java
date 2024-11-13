6efdef3dd91fbd7341d58bc27fb637edeb79e87c
SurajGardi <surajgardi0707@gmail.com>
2024-11-13 21:54:32 +0530

    string startsWith() added

---
+++ src/core_java/StringMethod_startsWith.java
@@ -0,0 +1,13 @@
+package core_java;
+
+public class StringMethod_startsWith {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="Welcome in java";
+		boolean result=s1.startsWith("W");//checks starting character and returns boolean value
+		System.out.println(result);
+	}
+
+}
