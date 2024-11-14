4a7befabf9713e35964f1806137207cdf03c46d8
SurajGardi <surajgardi0707@gmail.com>
2024-11-14 20:28:02 +0530

    string subString() added

---
+++ src/core_java/StringMethod_subString.java
@@ -0,0 +1,17 @@
+package core_java;
+
+public class StringMethod_subString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1="Java Programming";
+		System.out.println(s1.subSequence(3, 9));
+		System.out.println(s1.substring(6, 9));
+		System.out.println(s1.substring(7));
+
+		System.out.println(s1.substring(2,7));
+		System.out.println(s1.substring(2));
+
+	}
+}
