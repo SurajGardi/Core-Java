6ff107277e4b9dbdc257e26b66129ad6387cc0cf
SurajGardi <surajgardi0707@gmail.com>
2024-11-14 19:42:35 +0530

    string subSequence() added

---
+++ src/core_java/StringMethod_subSequence.java
@@ -0,0 +1,16 @@
+package core_java;
+
+public class StringMethod_subSequence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//the subsequence starts with the char value at the specified index &
+		//end with the char value at(end-1).
+		//returns char index
+		
+		String s1="Java Programming";
+		System.out.println(s1.subSequence(3, 9));
+	}
+
+}
