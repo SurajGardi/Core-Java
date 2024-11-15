b66bc6f5e6b8a12efa5b3065e0acec9dd5472e1f
SurajGardi <surajgardi0707@gmail.com>
2024-11-15 19:56:58 +0530

    string toLowerCase() added

---
+++ src/core_java/StringMethod_toLowerCase.java
@@ -0,0 +1,16 @@
+package core_java;
+
+public class StringMethod_toLowerCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//toLowerCase() ==> use to convert given string in lowerCase
+		
+		String s1="WELCOME in JAVA";
+		String s2 = s1.toLowerCase();
+		System.out.println(s2);
+		System.out.println(s2.toLowerCase());
+	}
+
+}
