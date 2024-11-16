e0fd680377cdff49d1c7b30254b2e32c76eb5b56
SurajGardi <surajgardi0707@gmail.com>
2024-11-16 23:44:44 +0530

    string toUpperCase() added

---
+++ src/core_java/StringMethod_toUpperCase.java
@@ -0,0 +1,15 @@
+package core_java;
+
+public class StringMethod_toUpperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//toUpperrCase() ==> use to convert given string in upperCase
+		
+		String s1="Welcome in java";
+		String s2 = s1.toUpperCase();
+		System.out.println(s2);
+	}
+
+}
