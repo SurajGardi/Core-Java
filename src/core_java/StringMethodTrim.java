be0019fd391af674f0ec0443a886f313b5325dbc
SurajGardi <surajgardi0707@gmail.com>
2024-11-17 15:45:14 +0530

    string trim() added

---
+++ src/core_java/StringMethodTrim.java
@@ -0,0 +1,13 @@
+package core_java;
+
+public class StringMethodTrim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//remover spaces before and after the string
+		String name="	Welcome Suraj	";
+		System.out.println(name.trim());
+	}
+
+}
